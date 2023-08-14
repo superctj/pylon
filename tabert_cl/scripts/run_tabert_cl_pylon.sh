cf156ea3fc9e6efbc8fabe507ac0e3d65a20dc94
superctj <congtj@umich.edu>
2023-08-14 01:10:10 -0400

    update scripts

--- tabert_cl/scripts/run_tabert_cl_pylon.sh
+++ tabert_cl/scripts/run_tabert_cl_pylon.sh
@@ -4,7 +4,6 @@
 set -Eeuo pipefail
 
 dataset_name="pylon"
-version="v3.2"
 instance_name="tabert_cl"
 ckpt_name="408_value_cl_epoch_19"
 num_samples=10
@@ -14,11 +13,11 @@ for k in $(seq 10 10 10)
 do
     python topk_search_pylon.py \
     --dataset_name=dataset_name \
-    --dataset_dir="/ssd/congtj/data/pylon_benchmark/${version}/source/" \
-    --query_file="/ssd/congtj/data/pylon_benchmark/${version}/all_queries.txt" \
-    --ground_truth_file="/ssd/congtj/data/pylon_benchmark/${version}/all_ground_truth.pkl" \
-    --index_dir="/ssd/congtj/pylon_artifacts/indexes/${instance_name}/${dataset_name}_${version}/" \
-    --output_dir="/home/congtj/pylon_metaspace/pylon/${instance_name}/results/${dataset_name}_${version}/" \
+    --dataset_dir="/ssd/congtj/data/pylon_benchmark/source" \
+    --query_file="/ssd/congtj/data/pylon_benchmark/all_queries.txt" \
+    --ground_truth_file="/ssd/congtj/data/pylon_benchmark/all_ground_truth.pkl" \
+    --index_dir="/ssd/congtj/pylon_artifacts/indexes/${instance_name}/${dataset_name}" \
+    --output_dir="/home/congtj/pylon_metaspace/pylon/${instance_name}/results/${dataset_name}" \
     --ckpt_path="/ssd/congtj/pylon_artifacts/checkpoints/${ckpt_name}.ckpt" \
     --embedding_dim=128 \
     --num_samples=${num_samples} \
--- wte_cl/scripts/run_wte_cl_pylon.sh
+++ wte_cl/scripts/run_wte_cl_pylon.sh
@@ -4,7 +4,6 @@
 set -Eeuo pipefail
 
 dataset_name="pylon"
-version="v3.2"
 instance_name="wte_cl"
 ckpt_name="420_wte_cl_epoch_9"
 num_samples=-1
@@ -14,11 +13,11 @@ for k in $(seq 10 10 100)
 do
     python topk_search_pylon.py \
     --dataset_name=dataset_name \
-    --dataset_dir="/ssd/congtj/data/pylon_benchmark/${version}/source/" \
-    --query_file="/ssd/congtj/data/pylon_benchmark/${version}/all_queries.txt" \
-    --ground_truth_file="/ssd/congtj/data/pylon_benchmark/${version}/all_ground_truth.pkl" \
-    --index_dir="/ssd/congtj/pylon_artifacts/indexes/${instance_name}/${dataset_name}_${version}/" \
-    --output_dir="/home/congtj/pylon_metaspace/pylon/${instance_name}/results/${dataset_name}_${version}/" \
+    --dataset_dir="/ssd/congtj/data/pylon_benchmark/source" \
+    --query_file="/ssd/congtj/data/pylon_benchmark/all_queries.txt" \
+    --ground_truth_file="/ssd/congtj/data/pylon_benchmark//all_ground_truth.pkl" \
+    --index_dir="/ssd/congtj/pylon_artifacts/indexes/${instance_name}/${dataset_name}" \
+    --output_dir="/home/congtj/pylon_metaspace/pylon/${instance_name}/results/${dataset_name}" \
     --encoder_path="/ssd/congtj/pylon_artifacts/web_table_embedding_models/web_table_embeddings_combo150.bin" \
     --ckpt_path="/ssd/congtj/pylon_artifacts/web_table_embedding_models/${ckpt_name}.ckpt" \
     --embedding_dim=64 \
